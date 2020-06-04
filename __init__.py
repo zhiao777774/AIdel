7fb0f06b9db7eef160314d00a06208118e0e2c65
zhiao777774 <47570633+zhiao777774@users.noreply.github.com>
2020-06-05 02:02:37 +0800

    新增建立環境模型函數

--- __init__.py
+++ __init__.py
@@ -7,6 +7,7 @@ from .speech_service import SpeechService
 from .detector import detect, BoundingBox
 from .obstacle_dodge_service import Dodger, generate_maze
 from .distance_measurementor import Calibrationor, Measurementor
+from .environmental_model import write_environmental_model
 
 _CALIBRATION_DISTANCE = 0
 _FOCALLEN = 0.0
@@ -41,8 +42,11 @@ def initialize():
         #frame = _image_preprocess(frame)
         #frame = _project_to_2d(frame)
         result, dets = detect(frame)
+        
         bboxes = []
-        if dets: bboxes = _calc_distance(result, dets)
+        if dets: 
+            bboxes = _calc_distance(result, dets)
+            write_environmental_model('data/environmentalModel.json', bboxes)
 
         cv2.namedWindow('result', cv2.WINDOW_NORMAL)
         cv2.imshow('result', result)
@@ -51,8 +55,8 @@ def initialize():
         if bboxes: 
             h = int(result.shape[0] / 2)
             w = result.shape[1]
-            
             maze = generate_maze(data = bboxes, height = h, width = w, benchmark = h, resolution = 90)
+
             dodger = Dodger(maze)
             dodger.calculate()
             dodger.print_maze()
@@ -109,7 +113,7 @@ def _measure_distance(calibration_distance, focallen, bbox):
     print(f'Distance in cm {distance}')
     
     return distance
-
+    
 def _signal_handle():
     _handler = lambda signal, frame: sys.exit(0)
 
--- environmental_model.py
+++ environmental_model.py
@@ -12,3 +12,30 @@ class EnvironmentalModel:
 
     def get(self, index):
         return self._data[index]
+
+def write_environmental_model(file_path, bboxes):
+    _to_str = lambda dic: dict(zip(
+            map(lambda k: k, dic.keys()),
+            map(lambda v: str(v), dic.values())
+        ))
+
+    json = []
+    for bbox in bboxes:
+        lt = _to_str(dict(bbox.coordinates.lt._asdict()))
+        rt = _to_str(dict(bbox.coordinates.rt._asdict()))
+        lb = _to_str(dict(bbox.coordinates.lb._asdict()))
+        rb = _to_str(dict(bbox.coordinates.rb._asdict()))
+
+        json.append({
+            'class': bbox.clsName,
+            'confidence': str(bbox.confidence),
+            'distance': str(bbox.distance),
+            'coordinate': {
+                'lt': lt,
+                'rt': rt,
+                'lb': lb,
+                'rb': rb,
+            }
+        })
+
+    fc.write_json(file_path, json)
