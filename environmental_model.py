9c428542c9fc4b2d069184377aa45a792e7e2e41
zhiao777774 <st1541154190@gmail.com>
2020-07-23 23:28:28 +0800

    新增socket類別及disconnect socket函數

--- environmental_model.py
+++ environmental_model.py
@@ -22,25 +22,49 @@ class EnvironmentalModel:
     def get(self, index):
         return self._data[index]
 
+
+class EnvironmentalModelSocket:
+    def __init__(self, host, port):
+        self._socket = socketio.Client()
+        self._socket.connect(f'http://{host}:{port}')
+        print('Environmental model socket is connecting.')
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, trace):
+        self.close()
+
+    def close(self):
+        print('Environmental model socket is closing.')
+        self._socket.disconnect()
+
+    def send(self, model):
+        self._socket.emit(
+            'receiveEnvironmentalModel', model)
+
+_MODEL_SOCKET = EnvironmentalModelSocket('120.125.83.10', '8090')
+
 def create_environmental_model(file_path, height, width, resolution, bboxes):
     _to_str = lambda dic: dict(zip(
             map(lambda k: k, dic.keys()),
             map(lambda v: str(v), dic.values())
         ))
 
-    json = {
+    model = {
         'height': height,
         'width': width,
         'resolution': resolution, 
         'obstacles' : []
     }
+
     for bbox in bboxes:
         lt = _to_str(dict(bbox.coordinates.lt._asdict()))
         rt = _to_str(dict(bbox.coordinates.rt._asdict()))
         lb = _to_str(dict(bbox.coordinates.lb._asdict()))
         rb = _to_str(dict(bbox.coordinates.rb._asdict()))
 
-        json['obstacles'].append({
+        model['obstacles'].append({
             'class': bbox.clsName,
             'confidence': str(bbox.confidence),
             'distance': str(bbox.distance),
@@ -52,29 +76,8 @@ def create_environmental_model(file_path, height, width, resolution, bboxes):
             }
         })
 
-    fc.write_json(file_path, json)
+    fc.write_json(file_path, model)
+    _MODEL_SOCKET.send(model)
 
-    with EnvironmentalModelSocket('120.125.83.10', '8090') as socket:
-        socket.send(json)
-
-
-class EnvironmentalModelSocket:
-    def __init__(self, host, port):
-        self._socket = socketio.Client()
-        self._socket.connect(f'http://{host}:{port}')
-        print('EnvironmentalModel socket is connecting.')
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, type, value, trace):
-        self.close()
-
-    def close(self):
-        print('EnvironmentalModel socket is closing.')
-        self._socket.disconnect()
-
-    def send(self, model):
-        self._socket.emit(
-            'receiveEnvironmentalModel', 
-            model)
+def disconnect_environmental_model_socket():
+    _MODEL_SOCKET.close()
