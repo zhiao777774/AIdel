5622b1bb9076ac1c4963472811fbd916dc6ccbb0
zhiao777774 <st1541154190@gmail.com>
2020-07-29 15:15:29 +0800

    新增GPS模組

--- sensor_module.py
+++ sensor_module.py
@@ -1,6 +1,8 @@
 import time
+import pynmea2
 import RPi.GPIO as GPIO
 from threading import Thread
+from serial import Serial
 from hcsr04sensor import sensor as hcsr04
 
 
@@ -12,7 +14,7 @@ class HCSR04(Thread):
 
         self.TRIGGER_PIN = trigger_pin
         self.ECHO_PIN = echo_pin
-        self._distance = -1
+        self._distance = None
 
     def run(self):
         while True:
@@ -21,7 +23,7 @@ class HCSR04(Thread):
                 raw_measurement = sr04.raw_distance()
                 distance = sr04.distance_metric(raw_measurement)
             except UnboundLocalError:
-                self._distance = -1
+                self._distance = None
                 continue
 
             print(f'超音波模組偵測到距離為 {distance}cm')
@@ -32,7 +34,42 @@ class HCSR04(Thread):
     @property
     def distance(self):
         return self._distance
+
+
+#GPS模組
+class GPS(Thread):
+    def __init__(self, port = '/dev/ttyAMA0'):
+        self._port = port
+        self._lat = None
+        self._lng = None
+
+    def run(self):
+        while True:
+            ser = Serial(self._port, baudrate=9600, timeout=0.5)
+            new_data = ser.readline()
             
+            if new_data[:6] == '$GPRMC':
+                new_msg = pynmea2.parse(new_data)
+                lat = new_msg.latitude
+                lng = new_msg.longitude
+                
+                print(f'Latitude: {str(lat)}, Longitude: {str(lng)}')
+                self._lat = str(lat)
+                self._lng = str(lng)
+
+                time.sleep(1)
+
+    def latlng(self):
+        return self._lat, self._lng
+
+    @property
+    def latitude(self):
+        return self._lat
+
+    @property
+    def longitude(self):
+        return self._lng
+
 
 #六自由度感測模組
 class LSM6DS3(Thread):
