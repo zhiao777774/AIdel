62069f5fda79a44d308b335c6700bf4577fe9ad1
zhiao777774 <st1541154190@gmail.com>
2020-08-18 22:42:38 +0800

    刪除多餘程式碼

--- image_processor.py
+++ image_processor.py
@@ -3,6 +3,7 @@ import numpy as np
 from imutils import perspective #imutils在pi上有出錯，記得修正
 from skimage.filters import threshold_local
 
+
 class ImageProcessor:
     def __init__(self, frame):
         self._frame = frame
@@ -74,17 +75,3 @@ class ImageProcessor:
     @property
     def frame(self):
         return self._frame
-
-def track():
-    # provide points from image 1
-    pts_src = np.array([[154, 174], [702, 349], [702, 572],[1, 572], [1, 191]])
-    # corresponding points from image 2 (i.e. (154, 174) matches (212, 80))
-    pts_dst = np.array([[212, 80],[489, 80],[505, 180],[367, 235], [144,153]])
-    # calculate matrix H
-    h, status = cv2.findHomography(pts_src, pts_dst)
-    # provide a point you wish to map from image 1 to image 2
-    a = np.array([[154, 174]], dtype='float32')
-    a = np.array([a])
-    # finally, get the mapping
-    pointsOut = cv2.perspectiveTransform(a, h)
-    print(pointsOut)
